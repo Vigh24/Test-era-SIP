cc5dc09f15817bdbae15722c09c597f628cb2c1c
vighnesh <vigneshpatil2486@gmail.com>
2024-06-15 12:49:39 +0530

    logs button added and updated

---
+++ LogsForm.Designer.cs
@@ -0,0 +1,41 @@
+﻿namespace SIPSample
+{
+    partial class LogsForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private ComponentFactory.Krypton.Toolkit.KryptonListBox listBoxLogs;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.listBoxLogs = new ComponentFactory.Krypton.Toolkit.KryptonListBox();
+            this.SuspendLayout();
+            // 
+            // listBoxLogs
+            // 
+            this.listBoxLogs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxLogs.Location = new System.Drawing.Point(0, 0);
+            this.listBoxLogs.Name = "listBoxLogs";
+            this.listBoxLogs.Size = new System.Drawing.Size(800, 450);
+            this.listBoxLogs.TabIndex = 0;
+            // 
+            // LogsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxLogs);
+            this.Name = "LogsForm";
+            this.Text = "Call Logs";
+            this.ResumeLayout(false);
+        }
+    }
+}
